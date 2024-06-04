4216ee1b4d78e8b852324b6c089168c439e9a27e
SarahBarbosa <sarah@fisica.ufc.br>
2024-06-04 16:46:02 -0300

    comparacao funcoes de prob

--- utils/justdoit.py
+++ utils/justdoit.py
@@ -1,5 +1,81 @@
+from typing import List
+from scipy.special import factorial
 import numpy as np
 import math
+
+def media_amostral(dados: List[float]) -> float:
+    """
+    Calcula a média amostral de cada variável aleatória.
+
+    Parâmetros
+    ----------
+    dados : list
+        Lista contendo os dados.
+
+    Retorna
+    -------
+    float
+        Média amostral.
+    """
+    return sum(dados) / len(dados)
+
+def variancia_amostral(dados: List[float]) -> float:
+    """
+    Calcula a variância amostral de cada variável aleatória.
+
+    Parâmetros
+    ----------
+    dados : list
+        Lista contendo os dados.
+
+    Retorna
+    -------
+    float
+        Variância amostral.
+    """
+    media = media_amostral(dados)
+    return sum((xi - media) ** 2 for xi in dados) / (len(dados) - 1)
+
+def covariancia_amostral(dados1: List[float], dados2: List[float]) -> float:
+    """
+    Calcula a covariância entre duas variáveis aleatórias.
+
+    Parâmetros
+    ----------
+    dados1 : list
+        Lista contendo os dados da primeira variável aleatória.
+    dados2 : list
+        Lista contendo os dados da segunda variável aleatória.
+
+    Retorna
+    -------
+    float
+        Covariância entre as duas variáveis aleatórias.
+    """
+    media1 = media_amostral(dados1)
+    media2 = media_amostral(dados2)
+    return sum((dados1[i] - media1) * (dados2[i] - media2) for i in range(len(dados1))) / (len(dados1) - 1)
+
+def correlacao_amostral(dados1: List[float], dados2: List[float]) -> float:
+    """
+    Calcula o coeficiente de correlação entre duas variáveis aleatórias.
+
+    Parâmetros
+    ----------
+    dados1 : list
+        Lista contendo os dados da primeira variável aleatória.
+    dados2 : list
+        Lista contendo os dados da segunda variável aleatória.
+
+    Retorna
+    -------
+    float
+        Coeficiente de correlação entre as duas variáveis aleatórias.
+    """
+    cov = covariancia_amostral(dados1, dados2)
+    dp_x = variancia_amostral(dados1) ** 0.5
+    dp_y = variancia_amostral(dados2) ** 0.5
+    return cov / (dp_x * dp_y)
 
 def normal_fdp(x: np.ndarray, mu: float, sigma: float) -> np.ndarray:
     """
@@ -93,3 +169,23 @@ def binomial_fmp(n: np.ndarray, N: int, p: float) -> np.ndarray:
     coef_binomial = np.array([math.comb(N, ni) for ni in n])
     return coef_binomial * (p ** n) * (q ** (N - n))
 
+def poisson_fmp(n: np.ndarray, mu: float) -> np.ndarray:
+    """
+    Calcula a função de massa de probabilidade (FMP) para uma distribuição de Poisson.
+
+    A fórmula da FMP da distribuição Poisson é dada por:
+    P_N(n) = (μ^n / n!) * e^(-μ)
+
+    Parâmetros
+    ----------
+    n : np.ndarray
+        O conjunto de valores (número de sucessos) para os quais a FMP será calculada.
+    mu : float
+        A média da distribuição de Poisson.
+
+    Retorna
+    -------
+    np.ndarray
+        O valor da FMP para cada valor no conjunto n dado.    
+    """
+    return (mu ** n / factorial(n)) * np.exp(-mu) 
